cd8714e40645242bcead37e1c9d5a1a09cc72421
Kuba Nadzieja <jameshopegit@gmail.com>
2021-10-13 23:41:23 +0200

    feat: added firebase config

---
+++ app/firebase/config/firebase.config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC-IBXsZJYqC53NfG7e01sLOP-iEMSq-Ss',
+  authDomain: 'pilkarzyki-3adbb.firebaseapp.com',
+  projectId: 'pilkarzyki-3adbb',
+  storageBucket: 'pilkarzyki-3adbb.appspot.com',
+  messagingSenderId: '242577815355',
+  appId: '1:242577815355:web:38fa9006f1119acc83291e',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
